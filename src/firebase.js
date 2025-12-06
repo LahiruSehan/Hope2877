b3b575589bc09a5329fa06c215772fcf85d192af
LahiruSehan <158451116+LahiruSehan@users.noreply.github.com>
2025-12-06 20:21:14 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD9Vn99DxI6L0yFrL9QAsu-tPWpO3TCk8w",
+  authDomain: "thelight-545c1.firebaseapp.com",
+  projectId: "thelight-545c1",
+  storageBucket: "thelight-545c1.firebasestorage.app",
+  messagingSenderId: "788058609882",
+  appId: "1:788058609882:web:910e17ff6ca0aae3702de3",
+  measurementId: "G-F7SE5BM0XL"
+};
