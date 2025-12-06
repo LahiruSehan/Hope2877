1903a03d932be91b1f4368ab3ba78b6a95dcdcba
LahiruSehan <158451116+LahiruSehan@users.noreply.github.com>
2025-12-06 21:57:59 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD9Vnp9DXI6LyF1Q9Asu-tPWpO3TCkBw",
+  apiKey: "AIzaSyCyaunkYk3-rtoDQ2EFB1M3f0BXWo_YBzE",
   authDomain: "thelight-545c1.firebaseapp.com",
   projectId: "thelight-545c1",
   storageBucket: "thelight-545c1.appspot.com",
