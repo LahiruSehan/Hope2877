714ae4e96f3cd1eed9d40a9eb3dd2af03a8c560a
LahiruSehan <158451116+LahiruSehan@users.noreply.github.com>
2025-12-07 13:31:43 +0530

    Update musicConfig.js

--- scripts/musicConfig.js
+++ scripts/musicConfig.js
@@ -3,9 +3,14 @@ window.MUSIC_CONFIG = {
 
     chapters: {
         1: [
-            { track: "assets/musics/intro.mp3", pages: [1, 7] },
-            { track: "assets/musics/battle.mp3", pages: [8, 15] },
-            { track: "assets/musics/sad.mp3", pages: [16, 22] }
+            { track: "assets/musics/Wasteland.mp3", pages: [1, 4] },
+            { track: "assets/musics/walkandhistory.mp3", pages: [5, 7] },
+            { track: "assets/musics/firstfight.mp3", pages: [8, 9] }
+            { track: "assets/musics/jeepwanted.mp3", pages: [10, 11] }
+            { track: "assets/musics/wanted.mp3", pages: [12, 14] }
+            { track: "assets/musics/Love.mp3", pages: [15, 18] }
+            { track: "assets/musics/Panic.mp3", pages: [19, 22] }
+            { track: "assets/musics/newworld.mp3", pages: [23, 24] }
         ]
     }
 };
