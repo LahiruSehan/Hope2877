8d3db1dfd1179a389a62348632c333de57d68946
LahiruSehan <158451116+LahiruSehan@users.noreply.github.com>
2025-12-08 05:41:12 +0000

    added chapter 2 with music

--- scripts/musicConfig.js
+++ scripts/musicConfig.js
@@ -16,6 +16,7 @@ window.MUSIC_CONFIG = {
             { track: "assets/musics/TheWastelandEdge.mp3", pages: [2, 5] },
             { track: "assets/musics/WhisperedHearts.mp3", pages: [6, 9] },
             { track: "assets/musics/WhispersofTomorrow.mp3", pages: [10, 11] },
+            { track: "assets/musics/emotional.mp3", pages: [12, 16] }
 
         ]
     }
